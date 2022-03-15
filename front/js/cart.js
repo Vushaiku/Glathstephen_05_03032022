2852b4309b2353cc1b125656b955d93e5788d39b
Stephen <glath.stephen@gmail.com>
2022-03-15 19:00:36 +0100

    modif qty

--- front/js/cart.js
+++ front/js/cart.js
@@ -19,7 +19,50 @@ panier.forEach(function (element, index) {
           document.getElementById("totalPrice").innerHTML = total;
           document.getElementById("totalQuantity").innerHTML = qtyProducts;
           var elt = document.getElementById("cart__items");
+          elt.innerHTML += `
+          <article class="cart__item" data-id="${product._id}" data-color="${product.color}">
+                  <div class="cart__item__img">
+                    <img src="${product.imageUrl}" alt="${product.altTxt}">
+                  </div>
+                  <div class="cart__item__content">
+                    <div class="cart__item__content__description">
+                      <h2>${product.name}</h2>
+                      <p>${element.color}</p>
+                      <p>${product.price} euros</p>
+                    </div>
+                    <div class="cart__item__content__settings">
+                      <div class="cart__item__content__settings__quantity">
+                        <p>Qté : ${element.qty}</p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${element.qty}">
+                      </div>
+                      <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+          `;
         }
       });
+    })
+    .then(function (res) {
+      loadListener();
     });
 });
+
+var selectElement = document.getElementsByName("itemQuantity");
+
+function loadListener() {
+  if (selectElement) {
+    console.log("Element trouvé");
+    for (var i = 0; i < selectElement.length; ++i) {
+      selectElement[i].addEventListener("change", changeQty);
+      console.log("Eventlistener ajouté pour produit n." + i);
+    }
+  } else console.log("Pas de produits !");
+}
+function changeQty(e) {
+  console.log("Nouvelle quantité ! (" + this.value + ")");
+  var close = this.closest("[data-id]");
+  console.log("Article à changer : " + close.dataset.id);
+}
